b687013284e0c2612e140c63ab0aeb2d66c03839
Larry Rix <ljr1981@msn.com>
2020-10-08 08:35:51 -0400

    post_script.sh mods for permission fails

--- post_script.sh
+++ post_script.sh
@@ -1,12 +1,19 @@
 #!/bin/sh
 # Post processing script
 
-#sudo chmod 777 generated_wrapper
+sudo chown -R larry generated_wrapper
+sudo chgrp -R larry generated_wrapper
+sudo chmod -R 777 generated_wrapper
 
 #copy Makefile
 cp Makefile.SH  ./generated_wrapper/c/src                  
 
+cp config.sh ./generated_wrapper/c/src
+cp /usr/local/Eiffel_19.05/studio/spec/linux-armv7/include/*.h ./generated_wrapper/c/include
+cp /usr/local/Eiffel_19.05/studio/spec/linux-armv7/include/*.decl ./generated_wrapper/c/include
+cp /usr/local/Eiffel_19.05/studio/spec/linux-armv7/include/*.interface ./generated_wrapper/c/include
+
 cd generated_wrapper/c/src/
 #sudo /usr/local/Eiffel_19.05/studio/spec/linux-armv7/bin/finish_freezing -library
-#/usr/local/Eiffel_19.05/studio/spec/linux-armv7/bin/finish_freezing -library
-finish_freezing -library
+/usr/local/Eiffel_19.05/studio/spec/linux-armv7/bin/finish_freezing -library
+#finish_freezing -library
